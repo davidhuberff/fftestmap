9aa710ec46b63fe5bd1c10f49d4f782b70b85db6
davidhuberff <77616559+davidhuberff@users.noreply.github.com>
2021-04-29 13:50:02 +0200

    Add files via upload

---
+++ mc_tiled_map/white_concrete.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="white_concrete" tilewidth="16" tileheight="16" tilecount="1" columns="1">
+ <image source="./white_concrete.png" width="16" height="16"/>
+</tileset>
